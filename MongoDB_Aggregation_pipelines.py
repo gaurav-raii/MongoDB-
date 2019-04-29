171946c5c1ce1fd7cc416f20597192e4a5a98033
Gaurav Rai <gauravrai@tamu.edu>
2019-04-29 13:45:30 -0500

    Create MongoDB_Aggregation_pipelines.py
    Added code to query MongoDb databases using aggregation pipeline framework.

---
+++ MongoDB_Aggregation_pipelines.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 29 13:04:07 2019
+
+@author: gaura
+"""
+# all these queries are run against a mongoDB instance provided with the "Data Wrangling with MongoDB" course at Udacity
+
+#we have to find the average regional city population for all countries in the cities collection.
+#we will first calculate the average city population for each region in a country
+#and then calculate the average of all the regional averages for a country
+
+from pymongo import MongoClient
+client = MongoClient('localhost:27017')
+db = client.examples
+
+
+
+pipeline = [{"$unwind":"$isPartOf"},
+            {"$group":{"_id":{"country": "$country","region":"$isPartOf"},"Avg_pop":{"$avg":"$population"}}},
+            {"$group":{"_id":"$_id.country", "avgRegionalPopulation":{"$avg":"$Avg_pop"}}}]
+    
+
+print(db.cities.aggregate(pipeline))
+
+
+# next we will find Which Region in India
+# that has the largest number of cities with longitude between 75 and 80
+
+pipeline = [{"$match":{"country":"India", "lon":{"$gt":75}, "lon":{"$lt":80}}},
+            {"$unwind":"$isPartOf"},
+            {"$group":{"_id":"$isPartOf", "count":{"$sum":1}}},
+            {"$sort":{"count":-1}},
+            {"$limit":1}
+                
+print(db.cities.aggregate(pipeline))
+
+# the average regional city population for all countries in the cities collection.
+
+pipeline = [{"$unwind":"$isPartOf"},
+                {"$group":{"_id":{"country": "$country","region":"$isPartOf"}, "Avg_pop":{"$avg":"$population"}}},
+                {"$group":{"_id":"$_id.country", "avgRegionalPopulation":{"$avg":"$Avg_pop"}}}]
+
+print(db.cities.aggregate(pipeline))
+
